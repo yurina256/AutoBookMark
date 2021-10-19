21b106934263737c17746572dedab8875cb6aeda
yurina256 <oudonbengosi@yahoo.co.jp>
2021-10-19 19:56:12 +0900

    init

---
+++ script.js
@@ -0,0 +1,23 @@
+function bookmark(link,title){
+    console.log("bookmark");
+    console.log(link,title);
+    chrome.bookmarks.create({
+        'parentId': bookmarkBar.id,
+        'title':title,
+        'url':link
+    });
+};
+
+window.alert(location.pathname);
+console.log("hogehuga");
+chrome.storage.local.get([location.pathname], function (result) {
+    if(result[location.pathname] == undefined){
+        chrome.storage.local.set({[location.pathname]:1}, function () {
+        });
+    }else{
+        chrome.storage.local.set({[location.pathname]:result[location.pathname]+1}, function () {
+        });
+        console.log(result[location.pathname]);
+        if(result[location.pathname] == 1){bookmark(location.pathname,document.title);}
+    }
+    });
